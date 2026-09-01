b635293447f3ece0d00cd755c4e303a846ec7e09
vpfjt567 <vpfjt567@naver.com>
2026-09-01 18:04:03 +0900

    20260901 first

---
+++ js/book.js
@@ -0,0 +1,83 @@
+
+async function fetchBooks(query) {
+    const REST_API_KEY = "fabf0d15ab33e4263ed790e6c0483958";
+    const params = new URLSearchParams({
+        target: 'title',
+        query
+    })
+
+    const url = `https://dapi.kakao.com/v3/search/book?${params}`
+
+    const response = await fetch(url, {
+        method: "GET",
+        headers: {
+            Authorization: `KakaoAK ${REST_API_KEY}`
+        }
+    });
+
+    console.log(response);
+
+    if (!response.ok) {
+        throw new Error(`HTTP 오류! 상태 코드:
+                ${response.status}`);
+    }
+
+    return await response.json();
+}
+
+async function bookData() {
+    const queries = [
+        { query: '공단기', sectionId: "slider" },
+        
+    ];
+
+    for (const { query, sectionId } of queries) {
+        const data = await fetchBooks(query);
+
+        const section = document.querySelector(`#${sectionId}`);
+        const boxElements = section.querySelectorAll(".swiper-slide");
+        console.log(section, boxElements);
+
+        boxElements.forEach((box, i) => {
+            const doc = data.documents[i];
+            console.log(box, i);
+            if (!doc) return;
+
+
+            box.innerHTML = `<img src="${doc.thumbnail}">
+                        <div class="bookinfo">
+                        <h3>${doc.title}</h3>
+                        <h6>${doc.authors}</h6>
+                        <p>${doc.contents.substring(0, 60)}</p>
+                        <button>click</button></div>
+                        `
+        });
+
+        var swiper = new Swiper('.mySwiper', {
+            navigation: {
+                nextEl: '#mainpage_page .swiper-button-next',
+                prevEl: '#mainpage_page .swiper-button-prev',
+            },
+            autoplay: {
+        delay: 3000, // 3000ms = 3초마다 다음 슬라이드로 이동
+        disableOnInteraction: false // 사용자가 버튼 클릭/스 와이프 후에도 자동 재생 유지
+    },
+            pagination: {
+                el: '.swiper-pagination',
+                clickable: true,
+                renderBullet: function (i, className) {
+                    // return '<img class="' + className + ' alt="bookimg" src="' + data.documents[i].thumbnail + '">';
+                    return `<img class="${className}" alt="bookimg" src="${data.documents[i].thumbnail}">`;
+                },
+            },
+            clickable: true,
+            mousewheel: true,
+            keyboard: true,
+        });
+    }
+
+
+}
+
+
+bookData();
